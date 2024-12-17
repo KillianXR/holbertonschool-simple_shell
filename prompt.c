869bf319d358fd25270abc15922a3c512a7c6c20
KillianXR <9756@holbertonstudents.com>
2024-12-17 15:17:13 +0100

    betty prompt.c

--- prompt.c
+++ prompt.c
@@ -8,23 +8,24 @@
 */
 int prompt(void)
 {
- char *line = NULL;
- size_t len = 0;
- ssize_t nbread;
-while (1)
- {
- /* Prints the prompt */
-  printf("$ ");
- /* Reads a line from the Standard Input */
-  nbread = getline(&line, &len, stdin);
- /* Breaks the loop if EOF is reached */
- if (nbread == -1)
- {
-  break;
- }
- /* Prints the input line */
- printf("%s", line);
- }
- free(line);
- return (0);
+	char *line = NULL;
+	size_t len = 0;
+	ssize_t nbread;
+
+	while (1)
+	{
+	/* Prints the prompt */
+		printf("$ ");
+	/* Reads a line from the Standard Input */
+		nbread = getline(&line, &len, stdin);
+	/* Breaks the loop if EOF is reached */
+	if (nbread == -1)
+	{
+		break;
+	}
+	/* Prints the input line */
+	printf("%s", line);
+	}
+	free(line);
+	return (0);
 }
