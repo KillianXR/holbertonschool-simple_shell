cfdddece73cee6367935a2faac477d9b095f1b21
Gabriel-Nct <bescondgabriel@gmail.com>
2025-01-06 19:49:42 +0100

    fixing betty

--- _execute.c
+++ _execute.c
@@ -8,22 +8,22 @@
  */
 int _execute(char **args)
 {
- pid_t child_pid;
- int status;
+	pid_t child_pid;
+	int status;
 	/* Déclaration de l'environnement */
- extern char **environ;
+	extern char **environ;
 
- if (args[0] == NULL)
-  return (1);
+	if (args[0] == NULL)
+		return (1);
 
- child_pid = fork();
- if (child_pid == -1)
- {
-  perror("Error");
-  return (1);
- }
- else if (child_pid == 0)
- {
+	child_pid = fork();
+	if (child_pid == -1)
+	{
+		perror("Error");
+		return (1);
+	}
+	else if (child_pid == 0)
+	{
 	if (execve(args[0], args, environ) == -1)
 	{
 	perror("/.simple_shell");
