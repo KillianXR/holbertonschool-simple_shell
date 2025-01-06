aac67b19e1d792a4db99b94122408dbb6aeb3d0e
Gabriel-Nct <bescondgabriel@gmail.com>
2025-01-06 19:33:41 +0100

    Fixing indentation and adding a newline at the end

--- _execute.c
+++ _execute.c
@@ -8,31 +8,33 @@
  */
 int _execute(char **args)
 {
-    pid_t child_pid;
-    int status;
-    extern char **environ;  /* Déclaration de l'environnement */
+ pid_t child_pid;
+ int status;
+	/* Déclaration de l'environnement */
+ extern char **environ;
 
-    if (args[0] == NULL)
-        return (1);
+ if (args[0] == NULL)
+  return (1);
 
-    child_pid = fork();
-    if (child_pid == -1)
-    {
-        perror("Error");
-        return (1);
-    }
-    else if (child_pid == 0)
-    {
-        if (execve(args[0], args, environ) == -1)
-        {
-            perror("/.simple_shell");
-            free(args);  /* Libération de args en cas d'échec */
-            exit(1);
-        }
-    }
-    else
-    {
-        wait(&status);
-    }
-    return (0);
+ child_pid = fork();
+ if (child_pid == -1)
+ {
+  perror("Error");
+  return (1);
+ }
+ else if (child_pid == 0)
+ {
+ if (execve(args[0], args, environ) == -1)
+  {
+   perror("/.simple_shell");
+   /* Libération de args en cas d'échec */   
+			free(args);
+   exit(1);
+  }
+ }
+ else
+ {
+  wait(&status);
+ }
+ return (0);
 }
