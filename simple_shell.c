2a71c27295fd83b7d59a7d7cc0b1e413bc035059
Gabriel-Nct <bescondgabriel@gmail.com>
2025-01-06 17:54:14 +0100

    Added a first version of Simple shell 0.1 with still a little memory leaky to resolve

---
+++ simple_shell.c
@@ -0,0 +1,43 @@
+#include "main.h"
+
+int main(void)
+{
+    char *line = NULL;
+    size_t len = 0;
+    ssize_t nread;
+    char **args;
+
+    while (1)
+    {
+        printf("❓ UnknownCommand> ");
+        nread = getline(&line, &len, stdin);
+        if (nread == -1)
+        {
+            free(line);
+												break;  /* Sortie de la boucle en cas d'erreur ou EOF */
+        }
+
+        if (line[nread - 1] == '\n')
+            line[nread - 1] = '\0';
+
+        if (strcmp(line, "exit") == 0)
+        {
+            free(line);
+												break;  /* Sortie de la boucle si la commande est "exit" */
+        }
+
+        args = _splitline(line);
+        _execute(args);
+								free(line);
+        free(args);
+    }
+
+    /* Assurez-vous que line est libéré avant de quitter */
+    if (line)
+    {
+        free(line);
+        line = NULL;
+    }
+				free(line);
+    return (0);
+}
