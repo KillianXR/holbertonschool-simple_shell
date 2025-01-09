60ca3e7bb0405bf4246c43347a1fc464c8024516
KillianXR <9756@holbertonstudents.com>
2025-01-09 10:48:14 +0100

    simple_shell.c interactive features

--- simple_shell.c
+++ simple_shell.c
@@ -1,4 +1,6 @@
 #include "main.h"
+
+#define PROMPT "$ " /* Définition de l'invite de commande */
 /**
 * main - simple UNIX command interpreter
 * This programms displays a prompt and wait an input
@@ -13,35 +15,35 @@ int main(void)
 	size_t len = 0;
 	ssize_t nread;
 	char **args;
+	int interactive;
+
+	interactive = isatty(STDIN_FILENO);
 
 	while (1)
 	{
-	/* Displays a prompt for the user */
-	printf("❓ UnknownCommand> ");
+		if (interactive)
+		{
+			write(STDOUT_FILENO, PROMPT, strlen(PROMPT));
+		}
 	nread = getline(&line, &len, stdin);
-	/* Exit the loop on error or EOF */
 	if (nread == -1)
 	{
 	free(line);
 	break;
 	}
-	/* Remove the new line character from input */
 	if (line[nread - 1] == '\n')
 		line[nread - 1] = '\0';
 
-	/* Exit the program if the command is exit */
 	if (strcmp(line, "exit") == 0)
 	{
 	free(line);
 	break;
 	}
-	/* Split the line into arguments then execute the command */
 	args = _splitline(line);
 	_execute(args);
 	free(line);
 	free(args);
 	}
-	/* Ensure memory is freed before exiting */
 	if (line)
 	{
 	free(line);
